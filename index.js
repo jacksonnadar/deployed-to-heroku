cc8e95c4899658483ea1e162de8bd94fc4eca2df
jacksonnadar <jacksonnadar123@gmail.com>
2020-02-19 13:49:24 +0530

    / changes

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", require("./router/datas"));
+app.use("/api/datas", require("./router/datas"));
 
 app.use("/api/users", require("./router/auth"));
 
